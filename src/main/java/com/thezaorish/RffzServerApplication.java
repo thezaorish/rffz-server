2c0b89a760999604053ced099936c34e1d83471a
Victor Balanica <victorbalanica@gmail.com>
2016-06-26 00:58:09 +0100

    initial import from Spring Initializr

---
+++ src/main/java/com/thezaorish/RffzServerApplication.java
@@ -0,0 +1,12 @@
+package com.thezaorish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RffzServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RffzServerApplication.class, args);
+	}
+}
