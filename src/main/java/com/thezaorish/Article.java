0dd48c0412e561160f63c3e2ee4b32eb1f17c9c5
Victor Balanica <victorbalanica@gmail.com>
2016-07-01 00:57:54 +0100

    a more complete article model

--- src/main/java/com/thezaorish/Article.java
+++ src/main/java/com/thezaorish/Article.java
@@ -9,6 +9,14 @@ public class Article extends ResourceSupport {
 
 	private String title;
 
+	private String description;
+
+	private String source;
+
+	private String publishDate;
+
+	private String imagePath;
+
 	private String url;
 
 	public String getTitle() {
@@ -16,6 +24,34 @@ public class Article extends ResourceSupport {
 	}
 	public void setTitle(String title) {
 		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getPublishDate() {
+		return publishDate;
+	}
+	public void setPublishDate(String publishDate) {
+		this.publishDate = publishDate;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
 	}
 
 	public String getUrl() {
@@ -29,6 +65,10 @@ public class Article extends ResourceSupport {
 	public String toString() {
 		return "Article{" +
 				"title='" + title + '\'' +
+				", description='" + description + '\'' +
+				", source='" + source + '\'' +
+				", publishDate='" + publishDate + '\'' +
+				", imagePath='" + imagePath + '\'' +
 				", url='" + url + '\'' +
 				'}';
 	}
--- src/main/java/com/thezaorish/ArticleController.java
+++ src/main/java/com/thezaorish/ArticleController.java
@@ -1,8 +1,5 @@
 package com.thezaorish;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityLinks;
-import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpEntity;
@@ -27,8 +24,8 @@ public class ArticleController {
 	@RequestMapping("/articles")
 	public HttpEntity<Resources<Article>> articles() {
 		List<Article> articles = new ArrayList<Article>();
-		articles.add(article("some example", "www.google.com"));
-		articles.add(article("another example", "www.wikipedia.com"));
+		articles.add(article1());
+		articles.add(article2());
 
 		Resources<Article> resources = new Resources<Article>(articles);
 		resources.add(linkTo(methodOn(ArticleController.class).articles()).withSelfRel());
@@ -38,7 +35,7 @@ public class ArticleController {
 
 	@RequestMapping("/articles/{id}")
 	public HttpEntity<Resource<Article>> article(@PathVariable Integer id) {
-		Article article = article("some example", "www.google.com");
+		Article article = article1();
 
 		Resource<Article> resource = new Resource<Article>(article);
 		resource.add(linkTo(methodOn(ArticleController.class).article(id)).withSelfRel());
@@ -46,10 +43,27 @@ public class ArticleController {
 		return new ResponseEntity<Resource<Article>>(resource, HttpStatus.OK);
 	}
 
-	private Article article(String title, String url) {
+	private Article article1() {
 		Article article = new Article();
-		article.setTitle(title);
-		article.setUrl(url);
+		article.setTitle("Echipa Stelei se reîntregește înaintea celui de-al doilea cantonament");
+		article.setDescription("Alex Chipciu a fost prezent astăzi la Institutul Național de Medicină Sportivă pentru a efectua vizita medicală.\n" +
+				"Mijlocașul se va alătura lotului antrenat de Laurențiu Reghecampf pentru cel de-al doilea cantonament din această vară, care va avea loc &amp;#238;n Olanda.\n" +
+				"Chipciu a venit singur la testele medicale și nu a discutat cu reprezentanții presei, notează Digi Sport. ...");
+		article.setImagePath("http://www9.gsp.ro/usr/thumbs/thumb_290_x_208/2016/06/30/741743-707555-chipciu.jpg");
+		article.setPublishDate("29-06-2016-21-00-00");
+		article.setSource("www.gsp.ro");
+		article.setUrl("http://www.gsp.ro/fotbal/liga-1/echipa-stelei-se-reintregeste-inaintea-celui-de-al-doilea-cantonament-482250.html");
+		return article;
+	}
+
+	private Article article2() {
+		Article article = new Article();
+		article.setTitle("A dat Steaua lovitura? &quot;Este cel mai bun transfer pe care l-a făcut în ultimii ani. E cel mai bun mijlocaş din Liga 1!&quot;");
+		article.setDescription("Ilie Dumitrescu este de părere că transferul lui Adnan Aganovic este cea mai importantă mutare făcută de Steaua în această vară. Fostul mijlocaş al Viitorului este văzut perechea ideală pentru Pintilii la mijlocul terenului.");
+		article.setImagePath("http://storage0.dms.mpinteractiv.ro/media/401/581/7946/15512227/1/6358502-mediafax-foto-alexandru-hojda.jpg");
+		article.setPublishDate("29-06-2016-18-54-47");
+		article.setSource("www.prosport.ro");
+		article.setUrl("http://feedproxy.google.com/~r/prosport/zWZD/~3/2_XjrwYIaqk/a-dat-steaua-lovitura-este-cel-mai-bun-transfer-pe-care-l-a-facut-in-ultimii-ani-e-cel-mai-bun-mijlocas-din-liga-1-15512227");
 		return article;
 	}
 
