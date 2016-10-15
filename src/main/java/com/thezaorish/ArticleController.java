3c5a7005db38939cd6261c84534ae873f5bc04c5
Victor Balanica <victorbalanica@gmail.com>
2016-10-16 00:44:37 +0100

    configuration of the paginated results with pagination links and self links

--- src/main/java/com/thezaorish/ArticleController.java
+++ src/main/java/com/thezaorish/ArticleController.java
@@ -1,49 +1,48 @@
 package com.thezaorish;
 
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.PagedResources.PageMetadata;
 import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.hateoas.Link.REL_NEXT;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.http.HttpStatus.OK;
 
-/**
- * Created by zaorish on 28/06/16.
- */
 @RestController
 public class ArticleController {
 
 	@RequestMapping("/articles")
-	public HttpEntity<Resources<Article>> articles() {
-		List<Article> articles = new ArrayList<Article>();
+	public HttpEntity<PagedResources<Resource<Article>>> articles(@RequestParam(required = false, defaultValue = "10") long size, @RequestParam(required = false, defaultValue = "1") long page) {
+		long totalElements = 31;
+
+		List<Resource<Article>> articles = new ArrayList<Resource<Article>>();
 		articles.add(article1());
 		articles.add(article2());
 
-		Resources<Article> resources = new Resources<Article>(articles);
-		resources.add(linkTo(methodOn(ArticleController.class).articles()).withSelfRel());
+		PagedResources<Resource<Article>> resources =  new PagedResources(articles, new PageMetadata(size, page, totalElements));
+		resources.add(linkTo(methodOn(ArticleController.class).articles(10, 1)).withSelfRel());
+		resources.add(linkTo(methodOn(ArticleController.class).articles(10, 2)).withRel(REL_NEXT));
 
-		return new ResponseEntity<Resources<Article>>(resources, HttpStatus.OK);
+		return new ResponseEntity<PagedResources<Resource<Article>>>(resources, OK);
 	}
 
-	@RequestMapping("/articles/{id}")
-	public HttpEntity<Resource<Article>> article(@PathVariable Integer id) {
-		Article article = article1();
-
-		Resource<Article> resource = new Resource<Article>(article);
-		resource.add(linkTo(methodOn(ArticleController.class).article(id)).withSelfRel());
-
-		return new ResponseEntity<Resource<Article>>(resource, HttpStatus.OK);
+	@RequestMapping("/articles/{url}")
+	public HttpEntity<Resource<Article>> article(@PathVariable String url) {
+		return new ResponseEntity<Resource<Article>>(article1(), OK);
 	}
 
-	private Article article1() {
+	private Resource<Article> article1() {
 		Article article = new Article();
 		article.setTitle("Echipa Stelei se reîntregește înaintea celui de-al doilea cantonament");
 		article.setDescription("Alex Chipciu a fost prezent astăzi la Institutul Național de Medicină Sportivă pentru a efectua vizita medicală.\n" +
@@ -53,10 +52,12 @@ public class ArticleController {
 		article.setPublishDate("29-06-2016-21-00-00");
 		article.setSource("www.gsp.ro");
 		article.setUrl("http://www.gsp.ro/fotbal/liga-1/echipa-stelei-se-reintregeste-inaintea-celui-de-al-doilea-cantonament-482250.html");
-		return article;
+
+		Link selfRel = linkTo(methodOn(ArticleController.class).article(article.getUrl())).withSelfRel();
+		return new Resource<Article>(article, selfRel);
 	}
 
-	private Article article2() {
+	private Resource<Article> article2() {
 		Article article = new Article();
 		article.setTitle("A dat Steaua lovitura? &quot;Este cel mai bun transfer pe care l-a făcut în ultimii ani. E cel mai bun mijlocaş din Liga 1!&quot;");
 		article.setDescription("Ilie Dumitrescu este de părere că transferul lui Adnan Aganovic este cea mai importantă mutare făcută de Steaua în această vară. Fostul mijlocaş al Viitorului este văzut perechea ideală pentru Pintilii la mijlocul terenului.");
@@ -64,7 +65,9 @@ public class ArticleController {
 		article.setPublishDate("29-06-2016-18-54-47");
 		article.setSource("www.prosport.ro");
 		article.setUrl("http://feedproxy.google.com/~r/prosport/zWZD/~3/2_XjrwYIaqk/a-dat-steaua-lovitura-este-cel-mai-bun-transfer-pe-care-l-a-facut-in-ultimii-ani-e-cel-mai-bun-mijlocas-din-liga-1-15512227");
-		return article;
+
+		Link selfRel = linkTo(methodOn(ArticleController.class).article(article.getUrl())).withSelfRel();
+		return new Resource<Article>(article, selfRel);
 	}
 
 }
